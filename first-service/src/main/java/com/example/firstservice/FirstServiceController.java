b7cc0050255d50b9624aabac877ca8a2576d9691
james-dev <chungjm0711@gmail.com>
2022-06-17 21:08:35 +0900

    getting port by env bean // webFlux can be used request or response (MVC) by using HttpServletRequest

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,10 +1,15 @@
 package com.example.firstservice;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 // http://localhost:8081/welcome
 // http://localhost:8081/first-service/welcome
@@ -12,6 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -26,7 +38,9 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First Service";
+    public String check(HttpServletRequest request) {  // webFlux 에서도 HttpServletRequest 선언만하면 사용가능
+        log.info("Server port={}", request.getServerPort());
+        return String.format("Hi, there. This is a message from First Service on PORT %s",
+                env.getProperty("local.server.port"));
     }
 }
