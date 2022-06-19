548973c37242c61d28c61c50fd8187e73e077e30
james-dev <chungjm0711@gmail.com>
2022-06-19 16:15:32 +0900

    feat : CustomUser extends User(spring security) to get userId using one query

---
+++ user-service/src/main/java/com/example/userservice/service/CustomUser.java
@@ -0,0 +1,20 @@
+package com.example.userservice.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class CustomUser extends User {
+
+    private final String userId;
+
+    public CustomUser(String username, String password, String userId, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+        this.userId = userId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+}
