0d6e47ef895e80defa42f0d65c8246cf1aad8cba
james-dev <chungjm0711@gmail.com>
2022-06-19 16:15:55 +0900

    feat : RequestLogin

---
+++ user-service/src/main/java/com/example/userservice/vo/RequestLogin.java
@@ -0,0 +1,18 @@
+package com.example.userservice.vo;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+public class RequestLogin {
+
+    @NotNull(message = "아이디 항목은 필수 입력값입니다.")
+    @Size(min = 2, message = "이메일은 최소 두 글자 이상입니다.")
+    private String email;
+
+    @NotNull(message = "패스워드 항목은 필수 입력값입니다.")
+    @Size(min = 8, message = "비밀번호는 최소 8 글자 이상입니다.")
+    private String password;
+}
